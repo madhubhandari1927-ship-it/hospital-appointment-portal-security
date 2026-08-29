7b4a9b3615c10bdb0264bb7b45be0490393393b7
Madhu Bhandari <madhu123@gmail.com>
2026-08-29 21:13:08 +0530

    Rotate Django secret key

--- cybercourse/hospital-appointment-portal/hospital/settings.py
+++ cybercourse/hospital-appointment-portal/hospital/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-***REMOVED***'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = '^uqf3*)sn&s6)o-1k528!5cun&n3$=qd3#*n2qc8^+x37=kx@#'# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
